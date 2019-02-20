efa6214b7767b44113eb2eaea216e3588f2aef23
lethosoict <letho.hust88@gmail.com>
2019-02-21 01:36:37 +0700

    Video 1,Min 18

---
+++ Unet_Server/Assets/Scripts/Server.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    private const int MAX_USER = 100;
+    private const int PORT = 26000;
+    private const int WEB_PORT = 26001;
+
+    private byte reliableChannel;
+    private int hostId;
+    private int webHostId;
+    private bool isStarted = false;
+
+    // Start is called before the first frame update
+
+    #region Monobehaviour
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+    #endregion
+
+    public void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_USER);
+
+        hostId = NetworkTransport.AddHost(topo, PORT, null);
+        webHostId = NetworkTransport.AddWebsocketHost(topo, WEB_PORT, null);
+
+        isStarted = true;
+    }
+}
